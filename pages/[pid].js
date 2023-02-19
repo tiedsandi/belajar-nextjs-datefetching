40988c5ca1bdc180d7c90d23d3eeafb097f9e431
tiedSandi <wkwkwkand@gmail.com>
2023-02-19 08:09:21 +0700

    fallback

--- pages/[pid].js
+++ pages/[pid].js
@@ -6,6 +6,10 @@ import { Fragment } from "react";
 export default function ProductDetailPage(props) {
     const { loadedProduct } = props
 
+    // if (!loadedProduct) {
+    //     return <p>loading...</p>
+    // }
+
     return (
         <Fragment>
             <h1>{loadedProduct.title}</h1>
@@ -36,9 +40,11 @@ export async function getStaticPaths() {
     return {
         paths: [
             { params: { pid: 'p1' } },
-            { params: { pid: 'p2' } },
-            { params: { pid: 'p3' } }
+            // { params: { pid: 'p2' } },
+            // { params: { pid: 'p3' } }
         ],
-        fallback: false
+        // fallback: true
+        fallback: 'blocking'
+
     };
 }

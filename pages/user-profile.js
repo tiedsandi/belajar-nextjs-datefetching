561f8d45cc80e2c76b7fa3f4b66a78aeecac33dd
tiedSandi <wkwkwkand@gmail.com>
2023-02-19 20:29:49 +0700

    fix error loadingproduct

--- pages/user-profile.js
+++ pages/user-profile.js
@@ -7,6 +7,8 @@ export default UserProfilePage
 export async function getServerSideProps(context) {
     const { params, req, res } = context;
 
+    // console.log('Server Side Code');
+
     return {
         props: {
             username: 'Max'
